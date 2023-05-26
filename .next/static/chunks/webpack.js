f992cce4c13d379c6895e80fae7cf8b3ccd04f4e
Samoge <samuelokwuchukwu4@gmail.com>
2023-05-26 15:24:22 +0100

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "942f8589ada88a87"; }
+/******/ 		__webpack_require__.h = function() { return "a8dca57c6446cbc4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {  animation: {
-      fadeIn: "fadeIn 3s ease-in forwards"
-      
-    },
-    keyframes: {
-      fadeIn: {
-        "0%": { opacity: 0 },
-        "100%": { opacity: 1 }
-      }
-    }},
-    variants: {
-      animation: ["motion-safe"]
-  }
   },
   plugins: [],
 }
